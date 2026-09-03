95f2f35bda89aebf19e029a74e616c9cccb9120a
flohms18 <trouilletflorent@outlook.com>
2026-09-03 12:07:35 +0200

    Download Clean CSV File

--- app.py
+++ app.py
@@ -10,3 +10,18 @@ uploaded_file = st.file_uploader("Upload a CSV file")
 if uploaded_file is not None : 
     uploaded_csv = pd.read_csv(uploaded_file)
     st.write(uploaded_csv)
+    agree = st.checkbox("Remove Duplicates?")
+    if agree:
+        new_uploaded_csv = uploaded_csv.drop_duplicates()
+        new_uploaded_csv_no = new_uploaded_csv.dropna()
+        st.write(new_uploaded_csv_no)
+        csv = new_uploaded_csv_no.to_csv(index=False).encode("utf-8")
+
+        st.download_button (
+            label="Download Clean CSV File",
+            data=csv,
+            file_name="data.csv",
+            mime="text/csv",
+            icon=":material/download:",
+        )
+        
