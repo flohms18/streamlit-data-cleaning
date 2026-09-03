5bbf16771e2047ccf7294060ffa76b5a2ae532d5
flohms18 <trouilletflorent@outlook.com>
2026-09-03 10:23:39 +0200

    Tested Pandas

--- app.py
+++ app.py
@@ -1,4 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+MyDf = pd.DataFrame({
+    'first column' : [1,2,3,4],
+    'second column': [10, 20, 30,40]
+})
+
 def hello():
-    print("Hello my streamlit installed works")
+    print(MyDf)
 
 hello()
